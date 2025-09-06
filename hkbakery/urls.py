72255627137de7edb239bac2d7a8a30100a43565
Chi Hong Edmund Chan <edmundchan@Chis-MacBook-Air.local>
2025-09-05 18:52:40 -0700

    Added delete and edit functionality

--- hkbakery/urls.py
+++ hkbakery/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_all_bread, name='get_all_bread')
+    path('', views.get_all_bread, name='get_all_bread'),
+    path('edit/<int:bread_id>/', views.edit_bread, name='edit_bread'),
+    path('delete/<int:bread_id>/', views.delete_bread, name='delete_bread')
 ]
--- hkbakery/views.py
+++ hkbakery/views.py
@@ -2,10 +2,43 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .services import BreadService
 from .forms import BreadForm
+from .models import Bread
 
 
 # Create your views here.
 def get_all_bread(request):
-    """ return all bread from database """
-    bread = BreadService.get_all_bread()
-    return render(request, 'get_all_bread.html', {'bread': bread})
+    
+    if request.method == 'POST':
+        form = BreadForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = BreadForm()
+        """ return all bread from database """
+        bread = BreadService.get_all_bread()
+        return render(request, 'get_all_bread.html', {'bread': bread, 'form': form})
+
+
+def edit_bread(request, bread_id):
+
+    bread = get_object_or_404(Bread, id=bread_id)
+
+    if request.method == 'POST':
+        form = BreadForm(request.POST, instance=bread)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = BreadForm(instance=bread)
+        return render(request, 'edit_bread.html', {'bread': bread, 'form': form})
+
+
+# take id of bread item and delete from db
+def delete_bread(request, bread_id):
+
+    item = get_object_or_404(Bread, id=bread_id)
+    if (request.method == 'POST'):
+        item.delete()
+        return redirect('/')
+    return redirect('/')
